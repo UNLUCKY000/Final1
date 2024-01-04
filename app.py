edeaaab708ff35b5fb740c1d66172eb801cd9fcb
UNLUCKY000 <62801726+UNLUCKY000@users.noreply.github.com>
2024-01-04 05:34:27 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ with st.sidebar:
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("Audio Story generator") 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Story is being generated"}]
 
 
 
@@ -17,11 +15,10 @@ if prompt := st.button("Generate"):
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
-    msg =  st.session_state["messages"] #"Write a story no more than 500 words"
+    msg =  [{"role": "assistant", "content": "Story is being generated"}]
     st.chat_message(msg["role"]).write(msg['content'])
     openai.api_key = openai_api_key
     msg.append({"role": "user", "content": "Write a story no more than 500 words"})
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=msg)
     msg = response.choices[0].message
     st.chat_message("assistant").write(msg.content)
-
