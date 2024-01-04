682de72e3246933d8f356ed4cd255974b72b809f
UNLUCKY000 <62801726+UNLUCKY000@users.noreply.github.com>
2024-01-04 04:29:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot") 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+st.title("Audio Story generator") 
+
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -19,7 +18,7 @@ if prompt := st.button("Generate"):
         st.stop()
 
     openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": "Write a story"})
+    st.session_state.messages.append({"role": "user", "content": "Write a story no more than 500 words"})
     #st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
